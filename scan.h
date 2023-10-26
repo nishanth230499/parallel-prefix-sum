9e07b6d7516a67bfd1b7a9f4c8df5b6fa89fd8d1
Nishanth P <nish4anth99@gmail.com>
2023-10-26 13:03:35 -0700

    completed soln3

--- scan.h
+++ scan.h
@@ -8,22 +8,14 @@ using namespace parlay;
  */
 template <typename T>
 T scan(T *A, size_t n) {
-  /**
-   * REPLACE THE BODY OF THE FOLLOWING
-   * FUNCTION WITH YOUR PARALLEL IMPLEMENTATION
-  */
-  
-  // note: 1) Extra functions are allowed 
-  //       2) DO NOT change the name of this `T scan(T *A, size_t n)`
-  // T total = 0;
   size_t k = 10000;
   size_t chunk_size = (size_t) n/k + 1;
   T *chunk_sum = (T *)malloc(k * sizeof(T));
 
   parallel_for(0, k, [&](size_t i) {
-    chunk_sum[k] = 0;
-    for(size_t j = 0; j < chunk_size && (k * chunk_size + j) < n; j++) {
-      chunk_sum[k] += A[k * chunk_size + j];
+    chunk_sum[i] = 0;
+    for(size_t j = 0; j < chunk_size && (i * chunk_size + j) < n; j++) {
+      chunk_sum[i] += A[i * chunk_size + j];
     }
   });
 
@@ -39,12 +31,15 @@ T scan(T *A, size_t n) {
 
   free(chunk_sum);
 
-  return A[n - 1];
+  for (size_t i = 1; i < chunk_size - 1; i++) {
+    A[i] += A[i-1];
+  }
 
-  // for (size_t i = 0; i < n; i++) {
-  //   T tmp = A[i];
-  //   A[i] = total;
-  //   total += tmp;
-  // }
-  // return total;
+  parallel_for(1, k, [&](size_t i) {
+    for(size_t j = 0; j < chunk_size - 1 && (i * chunk_size + j) < n - 1; j++) {
+      A[i * chunk_size + j] += A[i * chunk_size + j - 1];
+    }
+  });
+
+  return A[n - 1];
 }
--- scan_seq.h
+++ scan_seq.h
@@ -3,8 +3,8 @@ T seq_scan(T *A, size_t n) {
   T total = 0;
   for (size_t i = 0; i < n; i++) {
     T tmp = A[i];
-    A[i] = total;
     total += tmp;
+    A[i] = total;
   }
   return total;
 }
